969a5a63707a022e89aacda7c9b1e16acb5f29c5
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-09-20 19:52:47 -0700

    add helpers, hbs, etc

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// const helpers = require('./utils/helpers');
-// const hbs = exphbs.create({ helpers });
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -23,8 +23,8 @@ const sess = {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(session(sess));
 

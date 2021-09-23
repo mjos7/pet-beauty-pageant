b91c7d7d2af1009ae7a1075c7cb304aac4b4829d
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-09-22 18:52:28 -0700

    file structure for cloudinary. still needs fixing

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
+const upload = require('./controllers/uploads/multer')
+const cloudinary = require('./controllers/uploads/cloudinary');
+const fs = require('fs');
+const { url } = require('inspector');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -27,9 +31,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(session(sess));
+// app.use(bodyParser.urlencoded({extended:false}));
+// app.use(bodyParser.json());
+app.use('/upload-images',upload.array('image'),async(req, res)=>{
+  const uploader = async (path) => await cloudinary.uploads(path, 'Images')
+  if (req.method === 'POST') {
+    const urls = [];
+    const files = req.files;
+    
+    for(const files of files){
+      const { path } = file;
+
+      const newPath = await uploader(path);
+      url.push(newPath);
+      fs.unlinkSync(path);
+    }
+
+    res.status(200).json({
+      message: 'Images uploaded succesfully!',
+      data:urls
+    })
+  }
+  else{
+    res.status(405).json({
+      err:'Images not uploaded successfully.'
+    })
+  }
+});
 
 // turn on routes
-// app.use(routes);
+ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {

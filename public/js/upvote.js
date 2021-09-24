2aebbb586efe163ab30f8f81d0640acb6e4868fc
mjos7 <83667780+mjos7@users.noreply.github.com>
2021-09-23 17:38:21 -0700

    add upvote button and connect to js + fix hover states

--- public/js/upvote.js
+++ public/js/upvote.js
@@ -21,6 +21,29 @@ async function upvoteClickHandler(event) {
   }
 }
 
+async function upvoteClickHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+  const response = await fetch('/api/upvote', {
+    method: 'PUT',
+    body: JSON.stringify({
+      post_id: id,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert(response.statusText);
+  }
+}
+
 document
   .querySelector('.upvote-btn')
   .addEventListener('click', upvoteClickHandler);

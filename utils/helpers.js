4053c09e9f23d51577ecefa3972de2692d5064dd
Snow <84701509+snowden421@users.noreply.github.com>
2021-09-29 12:01:26 -0700

    fixed broken edit-post link

--- utils/helpers.js
+++ utils/helpers.js
@@ -13,12 +13,12 @@ module.exports = {
     return word;
   },
 
-  //   format_url: url => {
-  //     return url
-  //       .replace('http://', '')
-  //       .replace('https://', '')
-  //       .replace('www.', '')
-  //       .split('/')[0]
-  //       .split('?')[0];
-  //   },
+    format_url: url => {
+      return url
+        .replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .split('/')[0]
+        .split('?')[0];
+    },
 };
